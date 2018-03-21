120c25e44399badd4e308c37db1437a1736bf41a
PengJiyuan <731401082@qq.com>
2018-03-21 18:45:38 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 module.exports = less => {
   const lines = less.split('\n');
   const vars = {};
-  const reg = /^@(.*:[^;]*)/g;
+  const reg = /(^@(.*:[^]*));/;
   // -- @hr: 'for holder'
   const hrReg = /^\/{2}\s--\s*@(.*:[^;]*)/g;
   let matches = [];
 
   lines.forEach(line => {
-    if(line.match(reg)) {
-      matches = matches.concat(line.match(reg));
+    if(line.match(reg) && line.match(reg)[1]) {
+      matches = matches.concat(line.match(reg)[1]);
     } else if(line.match(hrReg)) {
       matches = matches.concat(line.match(hrReg));
     }
   });
 
   matches.forEach(m => {
-    const v = m.split(' @').length > 1 ? m.split(' @')[1] : m;
+    const v = m.split(' -- @').length > 1 ? m.split(' -- @')[1] : m;
     const [ key, value ] = v.split(':');
-    vars[key] = value.trim();
+
+    vars[key.trim()] = value.trim();
   });
 
   return vars;
